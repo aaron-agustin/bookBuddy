35ec657f95ccc8782e2775c90ad288874b567330
imrane-bendassi <146710804+ImraneBendassi@users.noreply.github.com>
2024-05-30 15:19:07 +0200

    ajout de commentaires explicites

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
+// Importer le composant StrictMode de la bibliothèque React
 import { StrictMode } from "react";
+
+// Importer la fonction createRoot du paquet react-dom/client
 import { createRoot } from "react-dom/client";
 
+// Importer le composant principal App depuis le fichier local App.js
 import App from "./App";
 
+// Obtenir l'élément HTML avec l'id "root" depuis le DOM
 const rootElement = document.getElementById("root");
+
+// Créer une racine pour l'application React en utilisant rootElement
 const root = createRoot(rootElement);
 
+// Rendre le composant App encapsulé dans le composant StrictMode de React dans l'élément root
+// StrictMode est un outil permettant de mettre en évidence les problèmes potentiels dans une application
 root.render(
   <StrictMode>
     <App />
